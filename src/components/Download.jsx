90c472a5dbbdb0dceac881456881e292b114b7f3
Anurag Affection <anuragaffection07@gmail.com>
2024-01-10 02:07:12 +0530

    updated

--- src/components/Download.jsx
+++ src/components/Download.jsx
@@ -5,24 +5,43 @@ import Education from '../components/previewComponents/Education'
 import Experience from '../components/previewComponents/Experience'
 import KeySkills from '../components/previewComponents/KeySkills'
 import Projects from '../components/previewComponents/Projects'
-import { Box } from '@mui/material'
+import { Paper } from '@mui/material'
+import jsPDF from 'jspdf'
+import Styles from '../styles/input.module.css'
+
 
 function Download() {
-    return (
-        <div>
-            <Box sx={{
-                display: 'flex',
-                flexDirection: 'column',
-                backgroundColor : '#f3f4f6',
-            }}>
-                <Bio />
-                <Experience/>
-                <Projects/>
-                <Education/>
-                <KeySkills />
-                <Address />
-            </Box>
 
+    const generatePDF = () => {
+        const report = new jsPDF('portrait', 'pt', 'a4');
+        report.html(document.querySelector('#report')).then(() => {
+            report.save('report.pdf');
+        });
+    }
+
+    return (
+        <div className={Styles.container} >
+            <div className={Styles.wrapper}>
+                <Paper
+                    elevation={3}
+                    id='report'
+                    sx={{
+                        display: 'flex',
+                        flexDirection: 'column',
+                        backgroundColor: '#f3f4f6',
+                        color: 'black',
+                        width : '600px'
+                    }}
+                >
+                    <Bio />
+                    <Experience />
+                    <Projects />
+                    <Education />
+                    <KeySkills />
+                    <Address />
+                </Paper>
+            </div>
+            <button className={Styles.button} onClick={generatePDF} type="button">Export PDF</button>
         </div>
     )
 }
