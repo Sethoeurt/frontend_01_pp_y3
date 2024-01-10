236d165e85d8dd04882f5ca0143cfeb2db5f2494
Anurag Affection <anuragaffection07@gmail.com>
2024-01-10 21:39:54 +0530

    home style updated

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -21,24 +21,35 @@ function Home() {
   return (
     <>
       <div className={Styles.container}>
-        {
-          templates.map((template) => {
-            return (
-              <div
-                key={template.id}
-                className={Styles.templatesImg}
-                onClick={() => fillDetails()}
-              >
-                <img
-                  src={template.template_img}
-                  alt={template.template_name}
-                  height={'380px'}
-                  width={'auto'}
-                />
-              </div>
-            )
-          })
-        }
+
+        <div className={Styles.headingWrapper}>
+          <div className={Styles.heading}> Resume Templates</div>
+          <div> Select a template to get started</div>
+        </div>
+
+        <div>
+          <div className={Styles.templateWrapper}>
+            {
+              templates.map((template) => {
+                return (
+                  <div
+                    key={template.id}
+                    className={Styles.templatesImg}
+                    onClick={() => fillDetails()}
+                  >
+                    <img
+                      src={template.template_img}
+                      alt={template.template_name}
+                      height={'380px'}
+                      width={'auto'}
+                    />
+                  </div>
+                )
+              })
+            }
+          </div>
+        </div>
+        
       </div>
     </>
   )
