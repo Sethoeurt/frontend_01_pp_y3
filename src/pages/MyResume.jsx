16a58bbbcf0ebf589b100c30453e12cdbea39248
Anurag Affection <anuragaffection07@gmail.com>
2024-01-22 12:21:58 +0530

    updated

--- src/pages/MyResume.jsx
+++ src/pages/MyResume.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
-
+import Download from '../components/Download.jsx'
 
 function MyResume() {
   return (
     <div>MyResume
-      
+      <div>
+        
+      </div>
     </div>
   )
 }
